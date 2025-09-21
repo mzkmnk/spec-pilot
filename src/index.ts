d59843b02c1bc778985981fc7f475e3880514ad3
mzkmnk <sma0729mex@gmail.com>
2025-09-21 13:26:04 +0900

    revert: index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { completable } from "@modelcontextprotocol/sdk/server/completable.js";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
-import { load } from "./utils/load.ts";
+import { load } from "./utils/load";
 
 export const server = new McpServer({
   name: "spec-pilot",
